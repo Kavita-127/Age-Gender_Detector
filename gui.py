b136e697f8bd18b6267cbf565b59ed695744333e
Kavita Pat Pingua <89763489+Kavita-127@users.noreply.github.com>
2023-07-20 18:34:21 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -14,11 +14,11 @@ model = load_model('Age_Sex_Detection.h5')
 top = tk.Tk()
 top.geometry('800x600')
 top.title('Age & Gender Detector')
-top.configure(background='#CDCDCD')
+top.configure(background='#a85454')
 
 # Initializing the labels (1 for age and 1 for sex)
-label1 = Label(top,background='#CDCDCD',font=('arial',15,'bold'))
-label2 = Label(top,background='#CDCDCD',font=('arial',15,'bold'))
+label1 = Label(top,background='#a85454',font=('arial',15,'bold'))
+label2 = Label(top,background='#a85454',font=('arial',15,'bold'))
 sign_image = Label(top)
 
 #Defining Detect function which detects the age and gender of the person in image using the model
@@ -38,13 +38,13 @@ def Detect(file_path):
     sex=int(np.round(pred[0][0]))
     print("Predicted Age is "+ str(age))
     print("Predicted Gender is "+sex_f[sex])
-    label1.configure(foreground="#011638",text=age)
-    label2.configure(foreground='#011638',text=sex_f[sex])
+    label1.configure(foreground="#110414",text=age)
+    label2.configure(foreground='#110414',text=sex_f[sex])
 
 #Defining Show_detect button function
 def show_Detect_button(file_path):
     Detect_b=Button(top,text="Detect Image",command=lambda: Detect(file_path),padx=10,pady=5)
-    Detect_b.configure(background='#364156',foreground='white',font=('arial',10,'bold'))
+    Detect_b.configure(background='#110414',foreground='white',font=('arial',10,'bold'))
     Detect_b.place(relx=0.79,rely=0.46)
 
 #Defining Upload Image function
@@ -63,13 +63,13 @@ def upload_image():
     except:
         pass
 
-upload=Button(top,text="Upload an Image",command=upload_image,padx=10,pady=5)
-upload.configure(background='#364156',foreground='white',font=('arial',10,'bold'))
+upload=Button(top,text="Upload Image",command=upload_image,padx=10,pady=5)
+upload.configure(background='#110414',foreground='white',font=('arial',10,'bold'))
 upload.pack(side='bottom',pady=50)
 sign_image.pack(side='bottom',expand=True)
 label1.pack(side='bottom',expand=True)
 label2.pack(side='bottom',expand=True)
 heading = Label(top,text="Age and Gender Detector",pady=20,font=('arial',20,"bold"))
-heading.configure(background="#CDCDCD",foreground="#364156")
+heading.configure(background="#a85454",foreground="#110414")
 heading.pack()
 top.mainloop()
